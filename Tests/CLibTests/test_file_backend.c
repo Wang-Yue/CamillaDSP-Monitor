ad7a40b55cd8f1316f3c2bce73d09ed2d090d277
Yue Wang <yuleopen@gmail.com>
2026-07-07 11:39:47 +0000

    Improve file backend test coverage

--- Tests/CLibTests/test_file_backend.c
+++ Tests/CLibTests/test_file_backend.c
@@ -228,4 +228,97 @@ TEST(FileBackendPauseThrottling) {
   remove(raw_filename);
 }
 
+static void run_format_roundtrip_test(binary_sample_format_t format, double eps) {
+  const char* raw_filename = "/tmp/test_file_backend_roundtrip_tmp.raw";
+  remove(raw_filename);
+
+  playback_device_config_t play_cfg;
+  memset(&play_cfg, 0, sizeof(play_cfg));
+  play_cfg.type = AUDIO_BACKEND_TYPE_FILE;
+  play_cfg.channels = 2;
+  snprintf(play_cfg.filename, sizeof(play_cfg.filename), "%s", raw_filename);
+  play_cfg.file_format = format;
+  play_cfg.is_wav = false;
+  play_cfg.has_filename = true;
+  play_cfg.has_file_format = true;
+  play_cfg.has_is_wav = true;
+
+  backend_error_t err;
+  playback_backend_t* playback =
+      file_playback_create(&play_cfg, 44100, 64, NULL, &err);
+  ASSERT_TRUE(playback != NULL);
+  ASSERT_TRUE(playback_backend_open(playback, &err));
+
+  audio_chunk_t* write_chunk = audio_chunk_create(10, 2);
+  for (size_t f = 0; f < 10; f++) {
+    audio_chunk_get_channel(write_chunk, 0)[f] = (double)f / 10.0;
+    audio_chunk_get_channel(write_chunk, 1)[f] = -(double)f / 10.0;
+  }
+  write_chunk->valid_frames = 10;
+
+  ASSERT_TRUE(playback_backend_write(playback, write_chunk, &err));
+  playback_backend_close(playback);
+  playback_backend_free(playback);
+
+  capture_device_config_t cap_cfg;
+  memset(&cap_cfg, 0, sizeof(cap_cfg));
+  cap_cfg.type = AUDIO_BACKEND_TYPE_FILE;
+  cap_cfg.channels = 2;
+  snprintf(cap_cfg.filename, sizeof(cap_cfg.filename), "%s", raw_filename);
+  cap_cfg.file_format = format;
+  cap_cfg.is_wav = false;
+  cap_cfg.has_filename = true;
+  cap_cfg.has_file_format = true;
+  cap_cfg.has_is_wav = true;
+
+  capture_backend_t* capture =
+      file_capture_create(&cap_cfg, 44100, 64, NULL, &err);
+  ASSERT_TRUE(capture != NULL);
+  ASSERT_TRUE(capture_backend_open(capture, &err));
+
+  audio_chunk_t* read_chunk = audio_chunk_create(10, 2);
+  ASSERT_TRUE(capture_backend_read(capture, 10, read_chunk, &err));
+  ASSERT_EQ(10, read_chunk->valid_frames);
+
+  for (size_t f = 0; f < 10; f++) {
+    ASSERT_NEAR((double)f / 10.0, audio_chunk_get_channel(read_chunk, 0)[f], eps);
+    ASSERT_NEAR(-(double)f / 10.0, audio_chunk_get_channel(read_chunk, 1)[f], eps);
+  }
+
+  audio_chunk_free(write_chunk);
+  audio_chunk_free(read_chunk);
+  capture_backend_close(capture);
+  capture_backend_free(capture);
+
+  remove(raw_filename);
+}
+
+TEST(FileBackendS16RoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_S16_LE, 1e-4);
+}
+
+TEST(FileBackendS24_3RoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_S24_3_LE, 1e-6);
+}
+
+TEST(FileBackendS24_4RJRoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_S24_4_RJ_LE, 1e-6);
+}
+
+TEST(FileBackendS24_4LJRoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_S24_4_LJ_LE, 1e-6);
+}
+
+TEST(FileBackendS32RoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_S32_LE, 1e-9);
+}
+
+TEST(FileBackendF32RoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_F32_LE, 1e-6);
+}
+
+TEST(FileBackendF64RoundTrip) {
+  run_format_roundtrip_test(BINARY_SAMPLE_FORMAT_F64_LE, 1e-12);
+}
+
 TEST_MAIN()
