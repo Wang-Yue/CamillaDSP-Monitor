8cf93ff989550cc0f3732a3a705e656d8060ecdd
Wang-Yue <yuleopen@gmail.com>
2026-07-17 00:22:24 +0900

    refactor: update DSP engine references to use CDSP functions for consistency

--- Sources/DSPLib/CDSPEngine.swift
+++ Sources/DSPLib/CDSPEngine.swift
@@ -10,13 +10,13 @@ import Foundation
 // MARK: - The actor
 
 extension Fader {
-  var cValue: fader_t {
+  var cValue: cdsp_fader_t {
     switch self {
-    case .main: return FADER_MAIN
-    case .aux1: return FADER_AUX1
-    case .aux2: return FADER_AUX2
-    case .aux3: return FADER_AUX3
-    case .aux4: return FADER_AUX4
+    case .main: return CDSP_FADER_MAIN
+    case .aux1: return CDSP_FADER_AUX1
+    case .aux2: return CDSP_FADER_AUX2
+    case .aux3: return CDSP_FADER_AUX3
+    case .aux4: return CDSP_FADER_AUX4
     }
   }
 }
@@ -28,12 +28,12 @@ public actor DSPEngine {
 
   public init() {
     print("[DSPEngine] Initializing C library engine...")
-    self.engine = dsp_engine_create()
+    self.engine = cdsp_engine_create()
   }
 
   deinit {
     if let e = engine {
-      dsp_engine_free(e)
+      cdsp_engine_free(e)
     }
   }
 
@@ -41,20 +41,18 @@ public actor DSPEngine {
 
   public func start(configJson: String) async throws {
     guard let e = engine else { return }
-    var err = audio_backend_error_t()
+    var err = cdsp_backend_error_t()
     let success = configJson.withCString { cStr in
-      dsp_engine_set_config(e, cStr, &err)
+      cdsp_set_config_json(e, cStr, &err)
     }
     if !success {
       let msg = withUnsafePointer(to: &err.message) { ptr in
         ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
       }
-      if err.type == AUDIO_BACKEND_ERR_COMMAND_SEND {
-        throw AudioBackendError.commandSend(message: msg)
-      } else if err.type == AUDIO_BACKEND_ERR_INVALID_SAMPLERATE {
-        throw AudioBackendError.invalidSamplerate(message: msg)
-      } else {
+      if err.type == CDSP_BACKEND_ERR_CONFIG_PARSE {
         throw AudioBackendError.configParse(message: msg)
+      } else {
+        throw AudioBackendError.commandSend(message: msg)
       }
     }
   }
@@ -65,64 +63,66 @@ public actor DSPEngine {
 
   public func stop() async {
     guard let e = engine else { return }
-    dsp_engine_stop(e)
+    cdsp_stop(e)
   }
 
   public func setFaderVolume(_ fader: Fader, _ db: Float) async {
     guard let e = engine else { return }
-    dsp_engine_set_fader_volume(e, fader.cValue, db, false)
+    cdsp_set_fader_volume(e, fader.cValue, db, false)
   }
 
   public func setFaderMute(_ fader: Fader, _ mute: Bool) async {
     guard let e = engine else { return }
-    dsp_engine_set_fader_mute(e, fader.cValue, mute)
+    cdsp_set_fader_mute(e, fader.cValue, mute)
   }
 
   public func getFaderVolume(_ fader: Fader) async -> Float {
     guard let e = engine else { return 0.0 }
-    return dsp_engine_get_fader_volume(e, fader.cValue)
+    return cdsp_get_fader_volume(e, fader.cValue)
   }
 
   public func isFaderMuted(_ fader: Fader) async -> Bool {
     guard let e = engine else { return false }
-    return dsp_engine_is_fader_muted(e, fader.cValue)
+    return cdsp_is_fader_muted(e, fader.cValue)
   }
 
   public func getStatus() async -> StateUpdate {
     guard let e = engine else { return StateUpdate(state: .inactive, stopReason: .none) }
-    let st = dsp_engine_get_status(e)
+    let st = cdsp_get_state(e)
     let state: ProcessingState
-    switch st.state {
-    case PROCESSING_STATE_RUNNING: state = .running
-    case PROCESSING_STATE_PAUSED: state = .paused
-    case PROCESSING_STATE_INACTIVE: state = .inactive
-    case PROCESSING_STATE_STARTING: state = .starting
-    case PROCESSING_STATE_STALLED: state = .stalled
+    switch st {
+    case CDSP_PROCESSING_STATE_RUNNING: state = .running
+    case CDSP_PROCESSING_STATE_PAUSED: state = .paused
+    case CDSP_PROCESSING_STATE_INACTIVE: state = .inactive
+    case CDSP_PROCESSING_STATE_STARTING: state = .starting
+    case CDSP_PROCESSING_STATE_STALLED: state = .stalled
     default: state = .inactive
     }
+    var rawStopReason = cdsp_stop_reason_t()
+    cdsp_get_stop_reason(e, &rawStopReason)
     let stopReason: ProcessingStopReason
-    switch st.stop_reason.type {
-    case STOP_REASON_NONE: stopReason = .none
-    case STOP_REASON_DONE: stopReason = .done
-    case STOP_REASON_CAPTURE_ERROR:
-      let msg = withUnsafePointer(to: st.stop_reason.message) { ptr in
-        ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
-      }
-      stopReason = .captureError(msg)
-    case STOP_REASON_PLAYBACK_ERROR:
-      let msg = withUnsafePointer(to: st.stop_reason.message) { ptr in
-        ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
-      }
-      stopReason = .playbackError(msg)
-    case STOP_REASON_CAPTURE_FORMAT_CHANGE:
-      stopReason = .captureFormatChange(Int(st.stop_reason.format_change_rate))
-    case STOP_REASON_PLAYBACK_FORMAT_CHANGE:
-      stopReason = .playbackFormatChange(Int(st.stop_reason.format_change_rate))
-    case STOP_REASON_UNKNOWN_ERROR:
-      let msg = withUnsafePointer(to: st.stop_reason.message) { ptr in
-        ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
-      }
-      stopReason = .unknownError(msg)
+    switch rawStopReason.type {
+    case CDSP_STOP_REASON_NONE: stopReason = .none
+    case CDSP_STOP_REASON_DONE: stopReason = .done
+    case CDSP_STOP_REASON_CAPTURE_ERROR:
+       let msg = withUnsafePointer(to: rawStopReason.message) { ptr in
+         ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
+       }
+       stopReason = .captureError(msg)
+    case CDSP_STOP_REASON_PLAYBACK_ERROR:
+       let msg = withUnsafePointer(to: rawStopReason.message) { ptr in
+         ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
+       }
+       stopReason = .playbackError(msg)
+    case CDSP_STOP_REASON_CAPTURE_FORMAT_CHANGE:
+       stopReason = .captureFormatChange(Int(rawStopReason.format_change_rate))
+    case CDSP_STOP_REASON_PLAYBACK_FORMAT_CHANGE:
+       stopReason = .playbackFormatChange(Int(rawStopReason.format_change_rate))
+    case CDSP_STOP_REASON_UNKNOWN_ERROR:
+       let msg = withUnsafePointer(to: rawStopReason.message) { ptr in
+         ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
+       }
+       stopReason = .unknownError(msg)
     default: stopReason = .none
     }
     return StateUpdate(state: state, stopReason: stopReason)
@@ -132,8 +132,11 @@ public actor DSPEngine {
     guard let e = engine else {
       return VuLevels(playback_rms: [], playback_peak: [], capture_rms: [], capture_peak: [])
     }
-    var levels = dsp_engine_get_vu_levels(e)
-    defer { dsp_engine_free_vu_levels(&levels) }
+    var levels = cdsp_vu_levels_t()
+    guard cdsp_get_vu_levels(e, &levels) else {
+      return VuLevels(playback_rms: [], playback_peak: [], capture_rms: [], capture_peak: [])
+    }
+    defer { cdsp_free_vu_levels(&levels) }
     let pbRms = Array(
       UnsafeBufferPointer(start: levels.playback_rms, count: Int(levels.playback_channels))
     ).map { Float($0) }
@@ -164,32 +167,29 @@ public actor DSPEngine {
     guard let e = engine else {
       throw AudioBackendError.engineNotRunning
     }
-    var res = spectrum_result_t()
-    let ch = channel != nil ? Int32(channel!) : -1
-    let st = dsp_engine_get_spectrum(e, isCapture, ch, minFreq, maxFreq, Int(nBins), &res)
-    if st != SPECTRUM_OK {
-      if st == SPECTRUM_ERROR_EMPTY {
-        throw AudioBackendError.bufferEmpty
-      } else {
-        throw AudioBackendError.spectrumCompute(message: "Spectrum compute error (\(st))")
-      }
+    var res = cdsp_spectrum_t()
+    let ch = channel ?? 0
+    let success = cdsp_get_spectrum(e, isCapture, ch, minFreq, maxFreq, Int(nBins), &res)
+    if !success {
+      throw AudioBackendError.bufferEmpty
     }
-    let freqs = Array(UnsafeBufferPointer(start: res.frequencies, count: Int(res.count)))
-    let mags = Array(UnsafeBufferPointer(start: res.magnitudes, count: Int(res.count)))
+    defer { cdsp_free_spectrum(&res) }
+    let freqs = Array(UnsafeBufferPointer(start: res.frequencies, count: Int(res.count))).map { Float($0) }
+    let mags = Array(UnsafeBufferPointer(start: res.magnitudes, count: Int(res.count))).map { Float($0) }
     return Spectrum(frequencies: freqs, magnitudes: mags)
   }
 
   public func getSamples(isCapture: Bool, nFrames: UInt32) async throws -> AudioSamples {
     guard let e = engine else { return AudioSamples(channels: [[], []]) }
-    var err = audio_backend_error_t()
-    guard let res = dsp_engine_get_samples(e, isCapture, Int(nFrames), &err) else {
-      if err.type == AUDIO_BACKEND_ERR_ENGINE_NOT_RUNNING {
+    var err = cdsp_backend_error_t()
+    guard let res = cdsp_get_samples(e, isCapture, Int(nFrames), &err) else {
+      if err.type == CDSP_BACKEND_ERR_UNKNOWN {
         throw AudioBackendError.engineNotRunning
       } else {
         throw AudioBackendError.bufferEmpty
       }
     }
-    defer { dsp_engine_free_samples(res) }
+    defer { cdsp_free_samples(res) }
     var channels: [[Float]] = []
     for ch in 0..<Int(res.pointee.channels_count) {
       if let ptr = res.pointee.channels[ch] {
@@ -204,15 +204,16 @@ public actor DSPEngine {
 
   public func getAvailableDevices(backend: String, input: Bool) async -> [AudioDevice] {
     guard engine != nil else { return [] }
-    var devs = [audio_device_t](repeating: audio_device_t(), count: 32)
-    let count = backend.withCString { cStr in
-      devs.withUnsafeMutableBufferPointer { ptr in
-        dsp_engine_get_available_devices(cStr, input, ptr.baseAddress, 32)
-      }
+    var devs: UnsafeMutablePointer<cdsp_device_info_t>? = nil
+    var count = 0
+    let success = backend.withCString { bStr in
+      cdsp_get_available_devices(bStr, input, &devs, &count)
     }
+    guard success, count > 0, let dPtr = devs else { return [] }
+    defer { free(dPtr) }
     var res: [AudioDevice] = []
-    for i in 0..<Int(count) {
-      let name = withUnsafePointer(to: devs[i].name) { ptr in
+    for i in 0..<count {
+      let name = withUnsafePointer(to: dPtr[i].name) { ptr in
         ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
       }
       res.append(AudioDevice(name: name))
@@ -226,15 +227,15 @@ public actor DSPEngine {
     isCapture: Bool
   ) async -> AudioDeviceDescriptor? {
     guard engine != nil else { return nil }
-    var devErr = device_error_t()
-    guard
-      let desc = backend.withCString({ bStr in
-        device.withCString { dStr in
-          dsp_engine_get_device_capabilities(bStr, dStr, isCapture, &devErr)
-        }
-      })
-    else {
-      if devErr.is_error {
+    var devErr = cdsp_device_error_t()
+    var outDesc: UnsafeMutablePointer<cdsp_device_descriptor_t>? = nil
+    let success = backend.withCString({ bStr in
+      device.withCString { dStr in
+        cdsp_get_device_capabilities(bStr, dStr, isCapture, &outDesc, &devErr)
+      }
+    })
+    guard success, let desc = outDesc else {
+      if devErr.type != CDSP_DEVICE_ERROR_NONE {
         let msg = withUnsafePointer(to: devErr.message) { ptr in
           ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
         }
@@ -242,7 +243,7 @@ public actor DSPEngine {
       }
       return nil
     }
-    defer { dsp_engine_free_device_capabilities(desc) }
+    defer { cdsp_free_device_capabilities(desc) }
     let name = withUnsafePointer(to: desc.pointee.name) { ptr in
       ptr.withMemoryRebound(to: CChar.self, capacity: 256) { String(cString: $0) }
     }
@@ -272,7 +273,7 @@ public actor DSPEngine {
 
   public func setLogLevel(_ level: LogLevel) async {
     level.rawValue.withCString { cStr in
-      dsp_engine_set_log_level(log_level_from_string(cStr))
+      cdsp_set_log_level(cStr)
     }
   }
 }
